ddde8c116ba42b66c7596045459596e98539a9d1
Moises Narvaez <moises.narvaez@koombea.com>
2017-03-23 12:03:41 -0500

    :wrench: Adding Components and Props samples

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+//import ComponentsAndProps from './react-doc/ComponentsAndProps.jsx';
 import './index.css';
 
 ReactDOM.render(
---
+++ src/react-doc/ComponentsAndProps.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+
+const Avatar = ({ url}) => (
+  <img alt='Profile' className='Avatar'
+    src={url} />
+);
+
+const User = ({ user })  => (
+  <div>
+    <h3>{user.name}</h3>
+    <Avatar url={user.avatarUrl}/>
+  </div>
+);
+
+const Skill = ({ name }) => (
+  <li>
+    {name}
+  </li>
+);
+
+const Skills = ({ skills }) => (
+  <ul className="skills_set">
+      {skills.map(skill => (
+        <Skill key={skill} name={skill} />
+      ))}
+  </ul>
+);
+
+const Profile = ({ person }) => (
+  <div className="profile">
+    <User user={person.user}/>
+    <Skills skills={person.skills}/>
+  </div>
+);
+
+const moises = {
+  user: {
+    name: 'Moi',
+    avatarUrl: 'http://graph.facebook.com/588121274/picture?type=square'
+  },
+  skills: ['Ruby on Rails', 'Angular JS', 'React JS']
+};
+
+class ComponentsAndProps extends Component {
+  render() {
+    return (
+      <Profile person={moises}/>
+    )
+  }
+}
+
+
+export default ComponentsAndProps;
+
+
